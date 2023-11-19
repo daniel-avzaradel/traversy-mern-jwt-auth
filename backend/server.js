a7bdbea8de9ec6cb53955c7364a6d445164fcd44
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-19 22:51:18 +0200

    added .env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
-const PORT = 5000;
+import dotenv from 'dotenv'
+dotenv.config();
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => res.send('server is ready'))
