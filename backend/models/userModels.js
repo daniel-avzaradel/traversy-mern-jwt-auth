8b64a69e1087e6eb0638e838af1c693d93a86dc2
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-20 02:25:06 +0200

    hasing password with bcrypt

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const UserSchema = mongoose.Schema(
   {
@@ -18,6 +19,15 @@ const UserSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model("User", UserSchema);
 export default User;
