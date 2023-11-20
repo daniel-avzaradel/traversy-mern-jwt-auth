6da377d0d7399a8cc79bd16db021b92e07bdc40d
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-20 02:04:55 +0200

    creating the user moodel with mongoose

---
+++ backend/models/userModels.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
