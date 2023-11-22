2707fc32f9c27f8ac774af8e9fbd8529a8f6fb02
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-23 00:55:22 +0200

    creating auth middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import colors from 'colors';
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
@@ -12,6 +14,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("server is ready"));
