90503993aa5d7172ad509e4b1976d82856b589bf
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-22 23:52:34 +0200

    compare passwords

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 
-const UserSchema = mongoose.Schema(
+const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
@@ -20,14 +20,18 @@ const UserSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-UserSchema.pre('save', async function (next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next()
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt)
-})
+});
 
-const User = mongoose.model("User", UserSchema);
+userSchema.methods.matchPasswords = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+const User = mongoose.model("User", userSchema);
 export default User;
