4afeadd94ae61e09480b92c1f4f5d10b9cc3b90e
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-26 01:06:29 +0200

    register user controller

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,35 +1,40 @@
-import asyncHandler from 'express-async-handler';
-import User from '../models/userModel';
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // Authenticate user
-const authUser = async (req, res) => {
-    console.log('User authorized');
-    res.status(200).send('auth user')
-}
+const authUser = asyncHandler(async (req, res) => {
+  console.log("User authorized");
+  res.status(200).send("auth user");
+});
 
-const registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-    const userExists = await User.findOne({ email });
+  const userExists = await User.findOne({ email });
 
-    if (userExists) {
-        res.status(400);
-        throw new Error('User already exists. Email already in use');
-    } 
-    
-    const user = await User.create({
-        name, email, password
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists. Email already in use");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
     });
-
-    if (user) {
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-        });
-    }
-
-};
+    console.log(`User registered`.white.underline, user);
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 const loginUser = async (req, res) => {
   console.log("login user");
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -14,7 +15,16 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-}, {timestamps: true});
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 const User = new mongoose.model('User', userSchema);
 export default User
