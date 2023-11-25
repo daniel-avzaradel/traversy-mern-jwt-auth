73aba30c2664991149bd92599cf5f9d5000614c0
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-25 22:39:49 +0200

    created middlewares

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,39 @@
+// Authenticate user
+
+const authUser = async (req, res) => {
+  console.log("auth user");
+};
+
+const loginUser = async (req, res) => {
+  console.log("login user");
+};
+
+const registerUser = async (req, res) => {
+  console.log("register user");
+};
+
+const getUserProfile = async (req, res) => {
+  console.log("get user profile");
+};
+
+const updateUserProfile = async (req, res) => {
+  console.log("update user profile");
+};
+
+const logoutUser = async (req, res) => {
+  console.log("logout user");
+};
+
+const deleteUser = async (req, res) => {
+  console.log("delete user");
+};
+
+export {
+  authUser,
+  loginUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  logoutUser,
+  deleteUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/users')
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`.cyan.underline);
