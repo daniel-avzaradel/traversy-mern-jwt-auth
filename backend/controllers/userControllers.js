7f248d437ab1f3cbb4842410c4ff488d521d4d4e
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-27 23:58:28 +0200

    creating controller functions

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,25 @@
+import asyncHandler from 'express-async-handler';
+
+const authUser = asyncHandler(async (req, res) => {
+    res.status(200).send('auth user')
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    res.status(200).send('user logged in')
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).send('user logged out')
+});
+
+const registerUser =  asyncHandler(async (req, res) => {
+    res.status(201).send('registered user')
+});
+
+const getUserProfile =  asyncHandler(async (req, res) => {
+    res.status(200).send('get user')
+});
+
+const updateUserProfile =  asyncHandler(async (req, res) => {
+    res.status(200).send('updated user')
+});
