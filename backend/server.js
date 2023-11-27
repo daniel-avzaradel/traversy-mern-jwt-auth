39db9801faab039c2e04d7793fba4bebe96e1109
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-27 23:54:27 +0200

    creating server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', (req, res) => {
+    console.log(`Server is running`.green);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.magenta);
+})
