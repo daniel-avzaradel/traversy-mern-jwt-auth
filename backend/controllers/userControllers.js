7cd84322a38b8d5737bf207f98d91822535a3ef9
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-28 00:59:58 +0200

    register user controller function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,9 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 const authUser = asyncHandler(async (req, res) => {
+    
+
     res.status(200).send('auth user')
 });
 
@@ -9,7 +12,33 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(201).send('registered user')
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('Email already in use');
+    } else {
+        
+        const user = await User.create({
+            name,
+            email,
+            password
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            })
+        } else {
+            res.status(400);
+            throw new Error('Invalid user data')
+        }
+        
+    }
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
