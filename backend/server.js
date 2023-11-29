1b4b87f1aceee33b87cb6781241ee4a016d37562
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-29 03:49:51 +0200

    error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import colors from 'colors';
 import dotenv from 'dotenv';
-import connectDB from "./db/db.js";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(notFound)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
