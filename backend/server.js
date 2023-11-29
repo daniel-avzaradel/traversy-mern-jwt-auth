75eea713be6c3c408abc31041cac0757a19a8aed
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-29 03:51:53 +0200

    added user routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from 'colors';
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
+import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -9,6 +10,8 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use('/api/users', userRoutes)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
