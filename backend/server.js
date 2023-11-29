029ad98cac356114c05e73624a2207895b4f42de
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-11-30 01:51:38 +0200

    error handlers middlewares

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
 
 import userRoutes from './routes/userRoutes.js'
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -14,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`.brightYellow);
