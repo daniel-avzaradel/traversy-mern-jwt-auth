f83d6c24e9b383e2a1066484ff7d25b892a94a53
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-01 21:01:47 +0200

    server.js

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,13 @@ import express from "express";
 import colors from 'colors';
 import dotenv from 'dotenv';
 
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.brightYellow.underline);
+});
