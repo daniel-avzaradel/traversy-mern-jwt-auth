cc072e7ac2f508714809dac236965165ca595623
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-04 01:15:56 +0200

    authenticating user with match passwords

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,11 +3,25 @@ import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js'
 
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).send('Auth User')
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPasswords(password))) {
+        generateToken(res, user._id)
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password')
+    }
+    
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-    
     const { name, email, password } = req.body;
 
     const userExists = await User.findOne({ email });
