39635d02bea36793724620a62ad478ed64474661
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-04 01:10:07 +0200

    fixed error handler and added token generator

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -5,7 +5,7 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-    let statusCode = req.statusCode === 200 ? 500 : req.statusCode;
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
 
     if (err.name === 'CastError' && err.kind === 'ObjectId') {
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,14 +2,14 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: 300
+        expiresIn: 60 * 30
     });
 
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: 'strict',
-        maxAge: 24 * 60 * 60 * 1000
+        maxAge: 60 * 1000 * 30
     });
 }
 
