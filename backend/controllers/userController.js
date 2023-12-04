0c4cdc1976b717183709bad7d8abb8b0124d345c
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-04 23:51:57 +0200

    changed matchPassword function name

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,7 @@ const authUser = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email });
 
-    if (user && (await user.matchPasswords(password))) {
+    if (user && (await user.matchPassword(password))) {
         generateToken(res, user._id)
         res.status(200).json({
             _id: user._id,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,7 +26,7 @@ userSchema.pre('save', async function (next) {
     }
 });
 
-userSchema.methods.matchPasswords = async function (enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 }
 
