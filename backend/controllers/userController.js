090f0d98445aad13c70c780a3bc3e673f74643fe
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-06 01:42:31 +0200

    remade authuser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,13 +2,32 @@ import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js'
 
+// const authUser = asyncHandler(async (req, res) => {
+//     const { email, password } = req.body;
+
+//     const user = await User.findOne({ email });
+
+//     if (user && (await user.matchPassword(password))) {
+//         generateToken(res, user._id)
+//         res.status(200).json({
+//             _id: user._id,
+//             name: user.name,
+//             email: user.email,
+//         });
+//     } else {
+//         res.status(401);
+//         throw new Error('Invalid email or password')
+//     }
+    
+// });
+
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))) {
-        generateToken(res, user._id)
+    if (user && (user.matchPassword(password))) {
+        generateToken(res, user._id);
         res.status(200).json({
             _id: user._id,
             name: user.name,
@@ -16,10 +35,9 @@ const authUser = asyncHandler(async (req, res) => {
         });
     } else {
         res.status(401);
-        throw new Error('Invalid email or password')
+        throw new Error('Invalid email or password');
     }
-    
-});
+})
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
