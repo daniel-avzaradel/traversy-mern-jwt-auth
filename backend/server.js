1de0eee87d5fd5e6bf172fa3b9fe0908705c60a0
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-05 22:28:52 +0200

    readded generate token function

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from 'colors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is connected on port: ${PORT}`.yellow.underline);
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,16 +1,16 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = (res, userId) => {
+const generateToken = async (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: 60 * 30
+        expiresIn: 300
     });
 
     res.cookie('jwt', token, {
+        maxAge: 90000,
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
-        maxAge: 60 * 1000 * 30
+        sameSite: 'strict'
     });
 }
 
-export default generateToken
+export default generateToken;
