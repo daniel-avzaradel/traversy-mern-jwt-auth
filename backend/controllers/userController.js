9db38ac129ae8fe86620f90c578dc60437e9c6d8
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-06 22:51:24 +0200

    added usermodel and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,29 @@
+import asyncHandler from "express-async-handler";
+
+const authUser = asyncHandler(async (req, res) => {
+    
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+    
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+    
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+}
