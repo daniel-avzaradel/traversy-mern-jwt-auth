b9f8251c29849661111833787f284cc56088b4af
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-06 23:03:02 +0200

    created generate Token function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,37 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 const authUser = asyncHandler(async (req, res) => {
     
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-    
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('Email already in use.')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from 'colors';
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
+import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from "./middlewares/errorHandling.js";
 
 dotenv.config();
@@ -11,6 +12,8 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
