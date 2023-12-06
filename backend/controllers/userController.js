1166911173f39d894b5821fe6f3ddc252985092e
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-06 02:00:00 +0200

    created generateToken, userSchema.methods.matchPassword and userSchema.pre('save)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,14 +2,73 @@ import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js'
 
+const authUser = asyncHandler(async (req, res) => {
 
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+        generateToken(res, user.email);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+    
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error('Email already in use');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        generateToken(res, user.email);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+});
 
 const logoutUsere = asyncHandler(async (req, res) => {
     res.status(200).send('Logout User')
 });
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).send('Get User')
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 const updateUserProfile = asyncHandler(async (req, res) => {
