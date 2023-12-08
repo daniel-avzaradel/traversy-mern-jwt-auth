1f5982b3bd5edfa52e3e3b7ff774535b6099e67f
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-08 22:25:03 +0200

    importing middlewares

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
 import userRoutes from './routes/userRoutes.js'
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`.cyan);
