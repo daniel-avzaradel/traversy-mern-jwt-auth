37cc55540509762b157e6b017e032e0c7ec8e3ae
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-23 23:37:27 +0200

    restarting server

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,14 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectDB();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-app.use('/api/users', userRoutes);
-
-app.use(notFound);
-app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`.yellow.underline);
