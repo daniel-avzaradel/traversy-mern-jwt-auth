34580167e3e37ea430468153e97add8f53ffcd10
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-23 23:55:16 +0200

    importing middlewares

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 
 import userRoute from './routes/userRoutes.js'
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 
@@ -13,6 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/users', userRoute)
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`.yellow.underline);
