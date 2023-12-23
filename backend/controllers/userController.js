273a433e7ddb1465c8be6ee64937485a6e44742a
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-24 00:16:20 +0200

    hashing password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,36 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 const authUser = asyncHandler(async (req, res) => {
     res.status(200).send('Auth user')
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(201).send('Register user')
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('Email already in use.');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data.')
+    }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
