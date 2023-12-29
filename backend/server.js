0b89529cc44e7529841b2a8ec257f5d716b9cc4c
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-12-29 22:33:45 +0200

    server js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import userRoute from './routes/userRoutes.js'
+import connectDB from './config/db.js';
+
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/users', userRoute);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`.yellow.underline);
+})
